da03b47ab2fc2ee58d10c0674ef813e7fb8aff55
Harsh <harshsantoshi05@gmail.com>
2024-04-02 15:18:20 +0530

    added apos

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-gradient-to-r from-primary-400 to-secondary-600 bg-clip-text">
-              Hello, I'm{" "}
+              Hello, I&apos;m{" "}
             </span>
 
             <br></br>
